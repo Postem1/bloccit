3028b9ba76af91223f186495f102ebc9e06f4259
Steeve Postemus <spostemus@gmail.com>
2016-03-09 17:05:32 -0800

    rails assignment testing done

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+
   describe "GET index" do
 
     it "renders the index template" do
@@ -13,6 +14,7 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+
   describe "GET about" do
 
     it "renders the about template" do
@@ -20,5 +22,14 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to render_template("about")
     end
   end
-  
+
+
+  describe "GET faq" do
+
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
+
 end
