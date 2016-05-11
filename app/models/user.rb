55da78f5c4aed27b79bed3da799d9b15ef8aee36
Steeve Postemus <spostemus@gmail.com>
2016-05-10 20:21:18 -0700

    checkpoint posts and users completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :posts
+
   before_save { self.email = email.downcase }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -7,7 +9,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
 
   validates :password, length: { minimum: 6 }, allow_blank: true
-  
+
   validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
