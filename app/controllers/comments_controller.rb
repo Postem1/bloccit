b73642d1ac4108116b3f5145af4f766daf5173ab
Steeve Postemus <spostemus@gmail.com>
2016-05-20 14:16:37 -0700

    destroy now avialable for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,9 @@ class CommentsController < ApplicationController
 
   before_action :require_sign_in
 
+  before_action :authorize_user, only: [:destroy]
+
+
   def create
     @post = Post.find(params[:post_id])
     comment = @post.comments.new(comment_params)
@@ -16,8 +19,30 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+     @post = Post.find(params[:post_id])
+     comment = @post.comments.find(params[:id])
+
+     if comment.destroy
+       flash[:notice] = "Comment was deleted successfully."
+       redirect_to [@post.topic, @post]
+     else
+       flash[:alert] = "Comment couldn't be deleted. Try again."
+       redirect_to [@post.topic, @post]
+     end
+   end
+
+
   private
   def comment_params
     params.require(:comment).permit(:body)
   end
+
+  def authorize_user
+   comment = Comment.find(params[:id])
+   unless current_user == comment.user || current_user.admin?
+     flash[:alert] = "You do not have permission to delete a comment."
+     redirect_to [comment.post.topic, comment.post]
+   end
+ end
 end
